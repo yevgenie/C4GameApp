7c3fa3122e272e903225d03e037e6891747f84c4
Eugene Kolesnik <ekolesnik@athenahealth.com>
2016-11-06 21:54:19 -0500

    - Completed Standard AI mode

--- scripts/c4App.js
+++ scripts/c4App.js
@@ -21,23 +21,65 @@ var Controllers;
         function GameController(_$scope, _c4Service) {
             this._$scope = _$scope;
             this._c4Service = _c4Service;
-            this.currentPlayer = Models.CellType.PlayerOne;
             this._moveCount = 0;
             this._$scope.vm = this;
-            this.msg = "Welcome to Connect 4 Game in Angular!";
+            this.msg = "Welcome to Connect 4 Game in AngularJS!";
             this.board = this._c4Service.createBoard(6, 7);
+            this.currentPlayer = Models.CellType.PlayerOne;
             this.gameMode = Models.GameMode.TwoPlayer;
+            this.gameOver = false;
             console.log(this.msg);
         }
-        GameController.prototype.changeGameMode = function (mode) {
+        GameController.prototype.startInGameMode = function (mode) {
             this.gameMode = mode;
             this.resetBoard();
             if (this.gameMode === Models.GameMode.VsAIStandard) {
-                // first pass, AI always makes first move to center
-                var firstMoveIndex = Math.floor(this.board[0].length / 2);
-                this.aiPlayer = this.currentPlayer;
-                this.makeMove(firstMoveIndex);
-                console.log("First Move Made by AI as Player " + this.aiPlayer);
+                this.msg = "Playing versus AI - Standard.";
+                this._aiPlayer = this.currentPlayer;
+                this._personPlayer = this._aiPlayer === Models.CellType.PlayerOne ? Models.CellType.PlayerTwo : Models.CellType.PlayerOne;
+                this.makeAiMove();
+                console.log("First Move Made by AI as Player " + this._aiPlayer);
+            }
+            else if (this.gameMode === Models.GameMode.VsAIHard) {
+                this.msg = "Sorry, hard mode not yet implemented.";
+            }
+            else {
+                this.msg = "Two player mode. Good luck!";
+            }
+        };
+        GameController.prototype.makeAiMove = function () {
+            if (!this.gameOver) {
+                var randomMove = Math.floor(Math.random() * 6);
+                var aiMoveIndex = this._moveCount === 0 ? Math.floor(this.board[0].length / 2) : randomMove;
+                // check for win-condition moves, if found, make it
+                for (var i = 0; i < this.board[0].length; i++) {
+                    var possibleMoveRowIndex = this.getNextAvailableRowIndex(i);
+                    if (possibleMoveRowIndex > -1) {
+                        // todo: clean up to re-use makeMove
+                        this.board[possibleMoveRowIndex][i] = this._aiPlayer;
+                        if (this._c4Service.checkWinConditions(this.board)) {
+                            this.endGame();
+                            return;
+                        }
+                        else {
+                            this.board[possibleMoveRowIndex][i] = Models.CellType.None;
+                        }
+                    }
+                }
+                // check for lose-condition moves open for oponnent, if found, make it
+                for (var i = 0; i < this.board[0].length; i++) {
+                    var possibleMoveRowIndex = this.getNextAvailableRowIndex(i);
+                    if (possibleMoveRowIndex > -1) {
+                        this.board[possibleMoveRowIndex][i] = this._personPlayer;
+                        if (this._c4Service.checkWinConditions(this.board)) {
+                            aiMoveIndex = i;
+                        }
+                        this.board[possibleMoveRowIndex][i] = Models.CellType.None;
+                    }
+                }
+                // pick random open move (not smart-mode)
+                console.log("AI Move to col. index: " + aiMoveIndex);
+                this.makeMove(aiMoveIndex);
             }
         };
         GameController.prototype.makeMove = function (columnIndex) {
@@ -46,15 +88,12 @@ var Controllers;
             if (rowIndex !== -1) {
                 this.board[rowIndex][columnIndex] = this.currentPlayer;
                 if (this._c4Service.checkWinConditions(this.board)) {
-                    this.anounceWinner();
+                    this.endGame();
                 }
                 this.currentPlayer = this.currentPlayer === Models.CellType.PlayerOne ? Models.CellType.PlayerTwo : Models.CellType.PlayerOne; // To support more than one player, this needs to be more robust
             }
-            if (this.gameMode === Models.GameMode.VsAIStandard && this.aiPlayer === this.currentPlayer) {
-                // check for win-condition moves, if found, make it
-                // check for lose-condition moves open for oponnent, if found, make it
-                // pick random open move (not smart-mode)
-                this.makeMove(Math.floor(this.board[0].length / 2));
+            if (this.gameMode === Models.GameMode.VsAIStandard && this._aiPlayer === this.currentPlayer) {
+                this.makeAiMove();
             }
         };
         GameController.prototype.getNextAvailableRowIndex = function (columnIndex) {
@@ -67,15 +106,19 @@ var Controllers;
             }
             return rowIndex;
         };
-        GameController.prototype.anounceWinner = function () {
+        GameController.prototype.endGame = function () {
             var color = this.currentPlayer.toString() === '1' ? 'RED' : 'BLUE'; // todo move to player:color assignment
-            alert(color + " wins!");
-            this.msg = color + " wins!";
-            this.resetBoard();
+            this.msg = "Player " + this.currentPlayer.toString() + "(" + color + ") wins!";
+            alert(this.msg);
+            this.gameOver = true;
         };
         GameController.prototype.resetBoard = function () {
+            this.gameOver = false;
             this._moveCount = 0;
             this.board = this._c4Service.createBoard(6, 7);
+        };
+        GameController.prototype.newGame = function () {
+            this.startInGameMode(this.gameMode);
         };
         return GameController;
     }());
--- typescript/Controllers.ts
+++ typescript/Controllers.ts
@@ -7,29 +7,77 @@ module Controllers {
         public msg: string;
         public board: Models.CellType[][];
         public gameMode: Models.GameMode;
-        public currentPlayer: Models.CellType = Models.CellType.PlayerOne;
-        private aiPlayer: Models.CellType;
+
+        public currentPlayer: Models.CellType;
+        private _aiPlayer: Models.CellType;
+        private _personPlayer: Models.CellType;
+
+        public gameOver: boolean;
         private _moveCount = 0;
-        
+
         constructor(private _$scope: any, private _c4Service: Services.C4Service) {
             this._$scope.vm = this;
-            this.msg = "Welcome to Connect 4 Game in Angular!";
+            this.msg = "Welcome to Connect 4 Game in AngularJS!";
             this.board = this._c4Service.createBoard(6, 7);
+            this.currentPlayer = Models.CellType.PlayerOne;
             this.gameMode = Models.GameMode.TwoPlayer;
+            this.gameOver = false;
             console.log(this.msg);
         }
 
-        public changeGameMode(mode: Models.GameMode) {
+        public startInGameMode(mode: Models.GameMode) {
             this.gameMode = mode;
             this.resetBoard();
 
-            if(this.gameMode === Models.GameMode.VsAIStandard)
+            if (this.gameMode === Models.GameMode.VsAIStandard) {
+                this.msg = "Playing versus AI - Standard.";
+                this._aiPlayer = this.currentPlayer;
+                this._personPlayer = this._aiPlayer === Models.CellType.PlayerOne ? Models.CellType.PlayerTwo : Models.CellType.PlayerOne;
+                this.makeAiMove();
+                console.log("First Move Made by AI as Player " + this._aiPlayer);
+            }
+            else if (this.gameMode === Models.GameMode.VsAIHard)
             {
-                // first pass, AI always makes first move to center
-                let firstMoveIndex = Math.floor(this.board[0].length / 2);
-                this.aiPlayer = this.currentPlayer;
-                this.makeMove(firstMoveIndex);
-                console.log("First Move Made by AI as Player " + this.aiPlayer);
+                this.msg = "Sorry, hard mode not yet implemented.";
+            }
+            else {
+                this.msg = "Two player mode. Good luck!";
+            }
+        }
+
+        private makeAiMove() {
+            if (!this.gameOver) {
+                let randomMove = Math.floor(Math.random() * 6);
+                let aiMoveIndex = this._moveCount === 0 ? Math.floor(this.board[0].length / 2) : randomMove;
+                // check for win-condition moves, if found, make it
+                for (let i = 0; i < this.board[0].length; i++) {
+                    let possibleMoveRowIndex = this.getNextAvailableRowIndex(i);
+                    if (possibleMoveRowIndex > -1) {
+                        // todo: clean up to re-use makeMove
+                        this.board[possibleMoveRowIndex][i] = this._aiPlayer;
+                        if (this._c4Service.checkWinConditions(this.board)) {
+                            this.endGame();
+                            return;
+                        }
+                        else {
+                            this.board[possibleMoveRowIndex][i] = Models.CellType.None;
+                        }
+                    }
+                }
+                // check for lose-condition moves open for oponnent, if found, make it
+                for (let i = 0; i < this.board[0].length; i++) {
+                    let possibleMoveRowIndex = this.getNextAvailableRowIndex(i);
+                    if (possibleMoveRowIndex > -1) {
+                        this.board[possibleMoveRowIndex][i] = this._personPlayer;
+                        if (this._c4Service.checkWinConditions(this.board)) {
+                            aiMoveIndex = i;
+                        }
+                        this.board[possibleMoveRowIndex][i] = Models.CellType.None;
+                    }
+                }
+                // pick random open move (not smart-mode)
+                console.log("AI Move to col. index: " + aiMoveIndex);
+                this.makeMove(aiMoveIndex);
             }
         }
 
@@ -39,17 +87,12 @@ module Controllers {
             if (rowIndex !== -1) {
                 this.board[rowIndex][columnIndex] = this.currentPlayer;
                 if (this._c4Service.checkWinConditions(this.board)) {
-                    this.anounceWinner();
+                    this.endGame();
                 }
                 this.currentPlayer = this.currentPlayer === Models.CellType.PlayerOne ? Models.CellType.PlayerTwo : Models.CellType.PlayerOne; // To support more than one player, this needs to be more robust
             }
-
-            if(this.gameMode === Models.GameMode.VsAIStandard && this.aiPlayer === this.currentPlayer)
-            {
-                // check for win-condition moves, if found, make it
-                // check for lose-condition moves open for oponnent, if found, make it
-                // pick random open move (not smart-mode)
-                this.makeMove(Math.floor(this.board[0].length / 2));
+            if (this.gameMode === Models.GameMode.VsAIStandard && this._aiPlayer === this.currentPlayer) {
+                this.makeAiMove();
             }
         }
 
@@ -66,16 +109,21 @@ module Controllers {
             return rowIndex;
         }
 
-        private anounceWinner() {
+        private endGame() {
             let color: string = this.currentPlayer.toString() === '1' ? 'RED' : 'BLUE'; // todo move to player:color assignment
-            alert(color + " wins!");
-            this.msg = color + " wins!";
-            this.resetBoard();
+            this.msg = "Player " + this.currentPlayer.toString() + "(" + color + ") wins!";
+            alert(this.msg);
+            this.gameOver = true;
         }
 
         private resetBoard() {
+            this.gameOver = false;
             this._moveCount = 0;
             this.board = this._c4Service.createBoard(6, 7);
+        }
+
+        public newGame() {
+            this.startInGameMode(this.gameMode);
         }
     }
 }
